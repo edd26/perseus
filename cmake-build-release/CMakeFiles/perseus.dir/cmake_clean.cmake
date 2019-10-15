2b7aa676aab4380cd0daedb6e565b9511006e178
edd26 <ed18abc@herts.ac.uk>
2019-10-15 16:21:36 +0100

    Realese compilation

---
+++ cmake-build-release/CMakeFiles/perseus.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/perseus.dir/Pers.cpp.o"
+  "perseus"
+  "perseus.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/perseus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
